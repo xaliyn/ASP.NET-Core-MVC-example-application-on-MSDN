50508527ca6fb2fabdd86da83dae82b5a80b56cc
xaliyn <xaliyn623@gmail.com>
2025-10-06 05:25:46 +0800

    Add validation

--- Migrations/20251005210859_Rating.cs
+++ Migrations/20251005210859_Rating.cs
@@ -1,4 +1,4 @@
-﻿5using Microsoft.EntityFrameworkCore.Migrations;
+﻿using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
@@ -8,6 +8,7 @@ namespace MvcMovie.Migrations
     public partial class Rating : Migration
     {
         /// <inheritdoc />
+        /*
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.AddColumn<string>(
@@ -15,6 +16,25 @@ namespace MvcMovie.Migrations
                 table: "Movie",
                 type: "nvarchar(max)",
                 nullable: true);
+        }
+        */
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // 1) Fix existing data first
+            migrationBuilder.Sql("UPDATE Movie SET Rating = 'NR' WHERE Rating IS NULL;");
+            migrationBuilder.Sql("UPDATE Movie SET Genre  = 'Unknown' WHERE Genre IS NULL;");
+            migrationBuilder.Sql("UPDATE Movie SET Title  = 'Untitled' WHERE Title IS NULL;");
+
+            // 2) Then make the columns non-nullable (if you changed annotations)
+            migrationBuilder.AlterColumn<string>(
+                name: "Rating",
+                table: "Movie",
+                type: "nvarchar(5)",
+                nullable: false,               // <- non-nullable now
+                oldClrType: typeof(string),
+                oldType: "nvarchar(5)",
+                oldNullable: true);
+            // Do similar AlterColumn for Genre/Title if you made them [Required]
         }
 
         /// <inheritdoc />
